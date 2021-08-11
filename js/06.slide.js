22a82be86afe4114aeaccb85a4d8da4b1f8e2602
SangYong-Jeong <mmaf@naver.com>
2021-08-11 17:04:23 +0900

    7교시 완료

--- js/06.slide.js
+++ js/06.slide.js
@@ -1,5 +1,27 @@
-var slide = new Slide('.wrapper', {
-	autoPlay: false,
-	effect: 'myStyle',
-	spped: 'myStyle'
+var slide = new Slide('#slide1', {
+	autoPlay: true,
+	effect: 'horizontal',
+	autoPlaySpeed: 1000,
+	navigation: {
+		prev: '.bt-prev',
+		next: '.bt-next',
+	},
+	pagination: {
+		el:'.pager'
+	},
+	speed: 500
+});
+
+var slide2 = new Slide('#slide2', {
+	autoPlay: true,
+	effect: 'vertical',
+	autoPlaySpeed: 2000,
+	speed: 800
+});
+
+var slide3 = new Slide('#slide3', {
+	autoPlay: true,
+	effect: 'fade',
+	autoPlaySpeed: 3000,
+	speed: 1000
 });
