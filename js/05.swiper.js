5d67448e39127dc0db8c6ffa6753b14350f3dd4f
SangYong-Jeong <mmaf@naver.com>
2021-08-11 11:21:55 +0900

    2교시 수업 완료

--- js/05.swiper.js
+++ js/05.swiper.js
@@ -1,6 +1,25 @@
 var swiper = new Swiper(".my-swiper", {
+	loop: true,
 	pagination: {
 		el: ".swiper-pagination",
-		dynamicBullets: true,
+		// dynamicBullets: true,
+		clickable: true,
 	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	}
+});
+
+var swiper = new Swiper(".my-swiper2", {
+	loop: true,
+	effect: "fade",
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	}
 });
